3ef05db90265b5baff67876fee240309de454cef
Karin Karlsson <karin.drake@gmail.com>
2020-05-07 22:50:29 +0200

    påbörjat css på searchcomp-cards

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,45 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './SearchStyle.css'
 
 const Search=({peopleData, planetsData})=>{
-
     
-   
-console.log(peopleData)
-console.log(planetsData)
+   const [category, setCategory]=useState('PEOPLE')
+    console.log(peopleData)
+    console.log(planetsData)
 
+    let jsxListPeople=peopleData.map((element)=>
 
-
-    // let jsxListPeople=peopleData.map((element)=>
-
-    //     <div key={element.name}>
-    //         <p>Name: {element.name}</p>
-    //         <p>Birthyear: {element.birth_year}</p>
-    //         <p>Haircolor: {element.hair_color}</p>
-    //         <p>Skincolor: {element.skin_color}</p>
-    //         <button>Lägg till i favoriter</button>
-    //     </div>
-    // )
+        <div key={element.name}  className='card'>
+            <h2>Name: {element.name}</h2>
+            <p>Birthyear: {element.birth_year}</p>
+            <p>Haircolor: {element.hair_color}</p>
+            <p>Skincolor: {element.skin_color}</p>
+            <button>Lägg till i favoriter</button>
+        </div>
+    )
 
     let jsxListPlanets=planetsData.map((planet)=>
 
-    <div key={planet.name}>
-        <p>Name: {planet.name}</p>
-        <p>Climate: {planet.climate}</p>
-        <p>Terrain: {planet.terrain}</p>
-        <button>Lägg till i favoriter</button>
-    </div>
-)
+        <div key={planet.name}  className='card'>
+            <h2>Name: {planet.name}</h2>
+            <p>Climate: {planet.climate}</p>
+            <p>Terrain: {planet.terrain}</p>
+            <button>Lägg till i favoriter</button>
+        </div>
+    )
+
+    let currentCategory=null;
+
+    switch(category){
+
+        case 'PEOPLE':
+            currentCategory=jsxListPeople
+            break;
+
+        case 'PLANETS':
+            currentCategory=jsxListPlanets
+            break;
+
+        default:
+            console.log('Sidan hittas ej')
+
+    }
 
 
     return(
 
-        <div>
+        <div className='search-comp'>
 
-            <button>People</button>
-            <button>Planets</button>
+            <button onClick={()=> setCategory('PEOPLE')}>People</button>
+            <button onClick={()=> setCategory('PLANETS')}>Planets</button>
 
-            {/* {jsxListPeople} */}
-            {jsxListPlanets}
+                <div className='gridContainer'>
+                    {currentCategory}
+                </div>
 
         </div>
     )
