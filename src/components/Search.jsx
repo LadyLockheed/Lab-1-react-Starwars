b33e2d0ed8dede3ce23dc07489f7324b4fbeb273
Karin Karlsson <karin.drake@gmail.com>
2020-05-07 22:27:28 +0200

    renderar alla planeter

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
 
 const Search=({peopleData, planetsData})=>{
+
+    
    
 console.log(peopleData)
-    let jsxListPeople=peopleData.map((element)=>
+console.log(planetsData)
 
-        <div key={element.name}>
-            <p>Name: {element.name}</p>
-            <p>Birthyear: {element.birth_year}</p>
-            <p>Haircolor: {element.hair_color}</p>
-            <p>Skincolor: {element.skin_color}</p>
-            <button>Lägg till i favoriter</button>
-        </div>
-    )
+
+
+    // let jsxListPeople=peopleData.map((element)=>
+
+    //     <div key={element.name}>
+    //         <p>Name: {element.name}</p>
+    //         <p>Birthyear: {element.birth_year}</p>
+    //         <p>Haircolor: {element.hair_color}</p>
+    //         <p>Skincolor: {element.skin_color}</p>
+    //         <button>Lägg till i favoriter</button>
+    //     </div>
+    // )
+
+    let jsxListPlanets=planetsData.map((planet)=>
+
+    <div key={planet.name}>
+        <p>Name: {planet.name}</p>
+        <p>Climate: {planet.climate}</p>
+        <p>Terrain: {planet.terrain}</p>
+        <button>Lägg till i favoriter</button>
+    </div>
+)
 
 
     return(
 
         <div>
 
-            {jsxListPeople}
+            <button>People</button>
+            <button>Planets</button>
+
+            {/* {jsxListPeople} */}
+            {jsxListPlanets}
 
         </div>
     )
