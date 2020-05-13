89955527b23839ec4b839b4282c59ee922a6844a
Karin Karlsson <karin.drake@gmail.com>
2020-05-14 01:34:33 +0200

    Updates

---
+++ precache-manifest.bd109dcff30a7b03218a4d43f4cc176a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d191ca5c1609954f2a5c6de8a383e6c0",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/index.html"
+  },
+  {
+    "revision": "cf995e39af631761260f",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/css/main.adf84b71.chunk.css"
+  },
+  {
+    "revision": "7736ad9ae5efcc2d3643",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/js/2.9c154d30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/js/2.9c154d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf995e39af631761260f",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/js/main.583b655c.chunk.js"
+  },
+  {
+    "revision": "23365dae3cf932151743",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/js/runtime-main.c6686f33.js"
+  },
+  {
+    "revision": "c61402428edb3b9bec45c1c66bdf2448",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/media/globalwhite.c6140242.png"
+  },
+  {
+    "revision": "eb47962666d561e9546f56a673b3642b",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/media/heartfilled.eb479626.png"
+  },
+  {
+    "revision": "eac7a17188096cf39fd0af7040cb03be",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/media/heartwhite.eac7a171.png"
+  },
+  {
+    "revision": "63ab0a12d02cbe38d004f59cbd84b2b9",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/media/magnifierblack.63ab0a12.png"
+  },
+  {
+    "revision": "6acae395a68692e2009d060e68f46719",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/media/socialwhite.6acae395.png"
+  },
+  {
+    "revision": "55de3598bfe0e683ebba0b779da90861",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/media/stormtrooper.55de3598.jpg"
+  },
+  {
+    "revision": "ffb32325b1388e29933b5d48bf242bf9",
+    "url": "/LadyLockheed/Lab-1-react-Starwars/static/media/stormtrooperonkeyboard.ffb32325.jpg"
+  }
+]);
